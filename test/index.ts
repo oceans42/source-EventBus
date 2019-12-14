7346de082592b653f646adfa9eaef731b90422ef
sparks42 <958575100@qq.com>
2019-12-14 21:58:55 +0800

    手写 EventHub

---
+++ test/index.ts
@@ -0,0 +1,39 @@
+import EventHub from "../src/index"
+
+type TestCase = (message: string) => void
+
+const test1: TestCase = message => {
+  const eventHub = new EventHub()
+  console.assert(eventHub instanceof Object === true, "eventHub 是个对象")
+  console.log(message)
+}
+
+const test2: TestCase = message => {
+  const eventHub = new EventHub()
+  let called = false
+  eventHub.on("xxx", (data) => {
+    called = true
+    console.assert(data === "今天天气不错")
+  })
+  eventHub.emit("xxx", "今天天气不错")
+  console.assert(called)
+  console.log(message)
+}
+
+const test3: TestCase = message => {
+  const eventHub = new EventHub()
+  let called = false
+  const fn1 = () => {
+    called = true
+  }
+
+  eventHub.on("yyy", fn1)
+  eventHub.off("yyy", fn1)
+  eventHub.emit("yyy")
+  console.assert(called === false)
+  console.log(message)
+}
+
+test1("EventHub 可以创建对象")
+test2(".on 了之后 .emit 会触发 .on 的函数")
+test3(".off 有用")
